c424a726066a0e4214fb95f347c86597808041c1
bertrand <bertrand.boullay@yahoo.fr>
2016-02-16 15:25:35 +0100

    Controller Classes done

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,51 @@
+class LessonsController < ApplicationController
+before_action :find_user, only: [ :new, :edit, :update, :create ]
+
+  def index
+    @lessons = Lesson.all
+  end
+
+  def show
+    @lesson = Lesson.find(params[:id])
+  end
+
+ def new
+    @lesson = Lesson.new
+  end
+
+  def create
+    @lesson = @user.lessons.build(lesson_params)
+    if @lesson.save
+    redirect_to user_lessons_path(@user)
+    else render :new
+    end
+  end
+
+  def update
+    if @lesson.update(lesson_params)
+      redirect_to user_lessons_path(@user)
+    else render :new
+    end
+  end
+
+  def edit
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def delete
+    @lesson = Lesson.find(params[:id])
+    @lesson.destroy
+    redirect_to user_lessons_path(@lesson)
+  end
+
+  private
+
+  def lesson_params
+  params.require(:lesson).permit(:name, :address, :price, :picture1)
+  end
+
+  def find_user
+    @user = User.find(params[:user_id])
+  end
+
+end
