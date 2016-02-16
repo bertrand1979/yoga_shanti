2257dc7c0bb9d061d79575a2004360a1902a13e5
Laurent Kretz <laurent.kretz@hotmail.fr>
2016-02-16 14:36:37 +0100

    link to profile page added in navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
 
   def show
+
 
   end
 
 private
+
+  def set_user
+    @user = User.find(params[:id])
+
+  end
 
 
 end
