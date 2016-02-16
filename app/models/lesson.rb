f6603df4777ac06b039f86a5f7b48ad2aecbc790
bertrand <bertrand.boullay@yahoo.fr>
2016-02-16 11:32:43 +0100

    crash test

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,5 @@
 class Lesson < ActiveRecord::Base
   belongs_to :user
+  has_many :sessions, dependent: :destroy
+  validates :name, :address, :price, :picture1, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :lessons
+  has_many :sessions, through: :lessons
+  has_many :events, class_name:'Session'
 end
