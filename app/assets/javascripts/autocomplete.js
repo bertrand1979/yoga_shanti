8f1d559e6a61eb800e142a737454f3603dcb370b
Laurent Kretz <laurent.kretz@hotmail.fr>
2016-03-31 15:33:19 +0200

    changing js google maps api

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,7 +1,7 @@
 function initializeAutocomplete(id) {
   var element = document.getElementById(id);
   if (element) {
-    var autocomplete = new google.maps.places.Autocomplete(element, { types: ['geocode'] });
+    var autocomplete = new.google.maps.places.Autocomplete(element, { types: ['geocode'] });
     google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
   }
 }
@@ -25,3 +25,4 @@ function onPlaceChanged() {
 google.maps.event.addDomListener(window, 'load', function() {
   initializeAutocomplete('user_input_autocomplete_address');
 });
+
